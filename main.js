c017b534b2dc261f1782aea8a59061c69d263e44
Alexander <alexanderjarl91@gmail.com>
2020-10-02 15:30:47 +0000

    todo list added

--- main.js
+++ main.js
@@ -6,15 +6,38 @@ window.addEventListener('load', () => {
 
     pads.forEach((pad, index) =>{
         pad.addEventListener('click', function(){
+
             sounds[index].currentTime = 0;
             sounds[index].play();
 
         });
+    });
+});
 
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound"); //This makes an array
+    const pads = document.querySelectorAll(".padRow1 div, .padRow2 div, .padRow3 div, .padRow4 div");
+    console.log(sounds)
+
+
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('keydown', function(e){
+            if (e.key === 'q') {
+                sounds[index].currentTime = 0;
+            sounds[index].play();
+            }
+
+        });
     });
 });
 
 
+//TO DO:
+//on click, give the div a background color of dark grey
+
+//flash push pads text
+
+//fancy animation intro
 
 //FLASH TEXT FUNCTION (NOT COMPLETELY WORKING)
 // window.addEventListener("load", function() {
